0c7053241cb81ad596a320178633f41772a3714a
RahmaaEsmail <108898486+RahmaaEsmail@users.noreply.github.com>
2023-02-20 07:58:36 +0200

    replace position absolute with fixed in navbar

--- js/script.js
+++ js/script.js
@@ -1,51 +1,49 @@
 const navBtn = document.querySelector('.btn-nav-menu');
 const header = document.querySelector(".header");
-navBtn.addEventListener('click',function()
-{
+navBtn.addEventListener('click', function () {
     header.classList.toggle('nav-open');
 })
 
 
 // scrolling smooth
 const allLinks = document.querySelectorAll('a:link');
-allLinks.forEach(function(link){
-    link.addEventListener("click",function(e){
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
         e.preventDefault();
         const href = link.getAttribute("href");
-        if(href==="#")
-        window.scrollTo({top:0,behavior:'smooth'})
-        if(href !=="#"&& href.startsWith("#"))
-        {
+        if (href === "#")
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        if (href !== "#" && href.startsWith("#")) {
             const sectionEl = document.querySelector(href);
-            sectionEl.scrollIntoView({behavior:"smooth"})
+            sectionEl.scrollIntoView({ behavior: "smooth" })
         }
 
-        if(link.classList.contains("nav--link"))
-        header.classList.toggle('nav-open');
+        if (link.classList.contains("nav--link"))
+            header.classList.toggle('nav-open');
     })
 })
 
 // change navbar bg while scrolling 
 // change bg
 function changeNavBarBg() {
-    if(window.pageYOffset === 0)
+    if (window.pageYOffset === 0)
         header.classList.remove("stikyBg")
-    else 
+    else
         header.classList.add("stikyBg")
 }
 
 
 let pause;
-const throttle = (cb,delay = 150)=>{
-   if(pause) return;
-   pause = true;
-   setTimeout (()=>{
-       cb();
-       pause = false;
-   },delay)
+const throttle = (cb, delay = 150) => {
+    if (pause) return;
+    pause = true;
+    setTimeout(() => {
+        cb();
+        pause = false;
+    }, delay)
 }
 
-window.addEventListener("scroll",()=>{
+window.addEventListener("scroll", () => {
     throttle(changeNavBarBg)
 })
 
